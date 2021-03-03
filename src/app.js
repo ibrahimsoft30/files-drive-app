f29e661af5655fd57f1b72486e6f3807b800c88f
ibrahimsoft30 <basam11er@gmail.com>
2021-03-03 19:05:45 +0300

    add helmetfor security

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
 const userRouter  = require('./routs/user');
 const fileRouter  = require('./routs/file');
 const { config } = require('../project-config/config');
@@ -11,6 +12,7 @@ const app = express();
 // paths
 const staticDir = path.join(__dirname,'../public');
 // middelwares
+app.use(helmet());
 app.use(express.json());
 app.use(express.static(staticDir));
 if(config.dev){
